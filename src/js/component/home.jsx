fb309f126ea399c87886e190365e092788b4be40
Frossting <109557017+Frossting@users.noreply.github.com>
2023-09-08 22:28:03 +0000

    finishedtask

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,14 +7,14 @@ import React from "react";
 const Home = (props) => {
 	console.log(props)
 	return (
-		<div className="Container d-flex justify-content-evenly my-5 bg-dark">
-			<div className="calendar">
+		<div className="container d-flex justify-content-evenly my-5 bg-dark justify-content-center" style= {{height:"200px"}}>
+			<div className="numbers calendar">
 				<i className="far fa-clock text-white"></i>
 			</div>
-			<div className="four text-white">{props.four % 10}</div>
-			<div className="three text-white">{props.three % 10}</div>
-			<div className="two text-white">{props.two % 10}</div>
-			<div className="one text-white">{props.one % 10}</div>
+			<div className="numbers four text-white">{props.four % 10}</div>
+			<div className="numbers three text-white">{props.three % 10}</div>
+			<div className="numbers two text-white">{props.two % 10}</div>
+			<div className="numbers one text-white">{props.one % 10}</div>
 		</div>
 	);
 };
